2961e406333e6b78b97fb2c865ab94106734ea88
KorkeshkoAlex <KorkeshkoAlex@mail.ru>
2018-06-27 16:51:36 +0300

    Добавьте файлы проекта.

---
+++ OSISP_Task_7/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace OSISP_Task_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelProductName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelProductName
+            // 
+            this.labelProductName.AutoSize = true;
+            this.labelProductName.Location = new System.Drawing.Point(0, 0);
+            this.labelProductName.Name = "labelProductName";
+            this.labelProductName.Size = new System.Drawing.Size(0, 13);
+            this.labelProductName.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.labelProductName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelProductName;
+    }
+}
+
---
+++ OSISP_Task_7/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Разработать программу, не допускающую запуск своего второго экземпляра.
+namespace OSISP_Task_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Timer timer = new Timer();
+            timer.Interval = 3000;
+
+            timer.Tick += new EventHandler(Tick);
+            timer.Enabled = true;
+            timer.Start();
+
+            string ProductNameText = "Имя продукта: " + Application.ProductName + "\n";
+            string ProcessNameText = "Имя процесса: " + Process.GetCurrentProcess().ProcessName + "\n";
+
+            this.labelProductName.Text += ProductNameText + ProcessNameText;
+        }      
+
+        private void Tick(object Sender, EventArgs e)
+        {
+            var FindProcess = Process.GetProcessesByName(Application.ProductName);
+            if (FindProcess.Length > 1)
+            {
+                for (int i = 1; i < FindProcess.Length; i++)
+                {
+                    Process pr = FindProcess[i];
+                    pr.CloseMainWindow();
+                }
+            }
+        }
+    }
+}
